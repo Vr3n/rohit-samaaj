0d463404df4c2b3ef71878d12c96e3fd38198dc3
Vr3n <vickyspatel@gmail.com>
2023-07-24 11:35:15 +0530

    Tailwind theming, and navbar implemented.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,36 @@
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          50: "#f5f6fa",
+          100: "#eaecf4",
+          200: "#cfd5e8",
+          300: "#a6b2d3",
+          400: "#7588bb",
+          500: "#4e6297",
+          600: "#415288",
+          700: "#36436e",
+          800: "#2f3a5d",
+          900: "#2b334f",
+          950: "#1d2134",
+        },
+        secondary: {
+          50: "#f8f5ee",
+          100: "#efe8d2",
+          200: "#e0d1a8",
+          300: "#ceb376",
+          400: "#bf9850",
+          500: "#b08442",
+          600: "#976a37",
+          700: "#79502f",
+          800: "#67432d",
+          900: "#58392b",
+          950: "#331d15",
+        },
+      },
+    },
   },
   plugins: [require("@tailwindcss/forms"), require("@tailwindcss/typography")],
 };
