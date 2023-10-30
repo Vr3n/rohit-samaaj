1a044b79eb755293ec355e7d606c95b34b2ab7d6
Vr3n <vickyspatel@gmail.com>
2023-10-30 22:15:36 +0530

    Schema for Personal Detail Form.

--- survey/api/schema.py
+++ survey/api/schema.py
@@ -1,4 +1,7 @@
+import re
+from typing import Union
 from ninja import Field, Schema
+from pydantic import validator
 
 
 class CountrySchema(Schema):
@@ -7,7 +10,7 @@ class CountrySchema(Schema):
 
 class StateBaseSchema(Schema):
     state: str
-    country: CountrySchema = None
+    country: str = Field(None, alias="country.country")
 
 
 class StateDistrictSchema(Schema):
@@ -17,3 +20,36 @@ class StateDistrictSchema(Schema):
 class DistrictSchema(Schema):
     district: str
     state: str = Field(None, alias="state.state")
+
+
+class SamaajMemberSchema(Schema):
+    first_name: str
+    last_name: str
+    father_name: str
+    mother_name: str
+
+
+class PersonalDetailFormSchema(Schema):
+    mobile_number: str
+    email_id: str
+    first_name: str
+    last_name: str
+    father_name: Union[str, None] = None
+    mother_name: Union[str, None] = None
+
+    @validator("mobile_number")
+    def mobile_number_must_contain_ten_digits(cls, m: str) -> str:
+        pattern = r'^\d{10}$'
+
+        if re.match(pattern, m):
+            raise ValueError("Enter Valid Mobile Number Format.")
+        return m
+
+    @validator("email_id")
+    def email_id_format(cls, e: str) -> str:
+        pattern = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,7}\b'
+
+        if re.match(pattern, e):
+            raise ValueError("Enter a valid email!")
+
+        return e
