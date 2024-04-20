3a950c684c0936bd6250dc0f07a34fa42a532877
Vr3n <vickyspatel@gmail.com>
2024-04-20 17:57:26 +0530

    .gitignore

--- survey/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.SurveyTermsPage.as_view(), name='survey-home'),
-]
--- survey/views.py
+++
@@ -1,8 +0,0 @@
-from django.views.generic import TemplateView
-
-
-# Create your views here.
-
-
-class SurveyTermsPage(TemplateView):
-    template_name = "survey/index.html"
